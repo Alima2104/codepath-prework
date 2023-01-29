3ecb7ccb2482786315de8834f6ca3a8eac95f9f8
Alima Zhagufarova <72438970+Alima2104@users.noreply.github.com>
2023-01-29 23:52:42 +0400

    added a improved version of the app

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # codepath-prework
 Introduce Yourself app
+App Description
+This app is designed for students to introduce themselves by providing their first name, last name, school name, year they are in, how many pets they have, and whether or not they want more pets. Clear interface is easy to use and navigate.
+
+App Walk-though
+TODO:// Add the URL to your animated app walk-though gif in the image tag below. Make sure the gif actually renders and animates when viewing this README. (☝️ Remove this paragraph after after adding gif). LICEcap is a great tool for recording and you can simply upload the gif to GitHub and provide the path name in place of URL as shown below.
+
+
+Required Features
+[x] 1. App displays an image of a school's logo
+[x] 2. App has three textfields for first, last, and school names
+[x] 3. App has a segmented control that changes student year
+[x] 4. Number of pet matches label is increased/decreased by stepper
+[x] 5. Switch makes a statement about wanting more pets or not(true/false)
+[x] 6. Introduce yourself button shows alert box with an introduciton and dismiss button
+
+Optional Features
+[x] 1. User can turn on/off the switch to change the color of the background view and text color
+[x] 2. When user swithes on the toggle to show that they want more pets, a few pers appear at the bottom of the screen.
+[x] 3. When not all text fields are filled and user presses the introduce myself button, a different alert box with reminder to fill out all text field appears.
+[x] 4. The dark mode is persistent, which means even after leaving the app it remembers whether user chose the dark theme or not (implemented using UserDefaults)
--- pleaseWork/ViewController.swift
+++ pleaseWork/ViewController.swift
@@ -5,10 +5,16 @@
 //  Created by NYUAD on 28/01/2023.
 //
 
+import SwiftUI
 import UIKit
 
 class ViewController: UIViewController {
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let isOn = UserDefaults.standard.bool(forKey: "DarkModeIsOn")
+       UpdateBGColor(isOn: isOn)
+    }
+    
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
     @IBOutlet weak var schoolTextField: UITextField!
@@ -17,28 +23,87 @@ class ViewController: UIViewController {
     @IBOutlet weak var numberOfPetsLabel: UILabel!
     @IBOutlet weak var morePetsStepper: UIStepper!
     @IBOutlet weak var morePetsSwitch: UISwitch!
+    @IBOutlet weak var cat: UIImageView!
+    @IBOutlet weak var dog: UIImageView!
+    @IBOutlet weak var rabbit: UIImageView!
+    @IBOutlet weak var turtle: UIImageView!
+    @IBOutlet weak var FirstName: UILabel!
+    @IBOutlet weak var lastName: UILabel!
+    @IBOutlet weak var school: UILabel!
+    @IBOutlet weak var year: UILabel!
+    @IBOutlet weak var pets: UILabel!
+    @IBOutlet weak var number: UILabel!
+    @IBOutlet weak var morePets: UILabel!
+    @IBOutlet weak var darkModeLabel: UILabel!
     
     @IBAction func stepperDidChange(_ sender: UIStepper) {
         numberOfPetsLabel.text = "\(Int(sender.value))"
     }
+    
+    @IBOutlet weak var darkModeSwitch: UISwitch!
+    
+    @IBAction func darkModeOn(_ sender: UISwitch) {
+        UpdateBGColor(isOn: sender.isOn)
+        UserDefaults.standard.setValue(sender.isOn, forKey: "DarkModeIsOn")
+    }
+    
+    @IBAction func morePets(_ sender: UISwitch) {
+        if (morePetsSwitch.isOn){
+            cat.isHidden=false;
+            dog.isHidden=false;
+            rabbit.isHidden=false;
+            turtle.isHidden=false;
+        }
+        else {
+            cat.isHidden=true;
+            dog.isHidden=true;
+            rabbit.isHidden=true;
+            turtle.isHidden=true;
+        }
+    }
+    
+    
     @IBAction func IntroduceSelfDidTapped(_ sender: UIButton) {
         let year = yearSegmentedControl.titleForSegment(at: yearSegmentedControl.selectedSegmentIndex);
                 
         let introduction = "My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend \(schoolTextField.text!). I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) dogs. It is \(morePetsSwitch.isOn) that I want more pets."
         
+        let warning = "Please fill out all fields"
+        
         let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
         
+        let alertController2 = UIAlertController(title: "Warning", message: warning, preferredStyle: .alert)
+        
         let action = UIAlertAction(title: "Nice to meet you!", style: .default, handler: nil)
+        let action2 = UIAlertAction(title: "Try Again", style: .default, handler: nil)
         
         alertController.addAction(action)
+        alertController2.addAction(action2)
                 
-        present(alertController, animated: true, completion: nil)
         
+        
+        if (firstNameTextField.text! == "" || lastNameTextField.text! == "" || schoolTextField.text! == "") {
+            present(alertController2, animated: true, completion: nil)
+        }
+        else {
+            present(alertController, animated: true, completion: nil)
+        }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    
+    private func UpdateBGColor(isOn: Bool){
+        view.backgroundColor = isOn ? .gray : .white;
+        FirstName.textColor = isOn ? .white : .black;
+        lastName.textColor = isOn ? .white : .black;
+        school.textColor = isOn ? .white : .black;
+        year.textColor = isOn ? .white : .black;
+        pets.textColor = isOn ? .white : .black;
+        morePets.textColor = isOn ? .white : .black;
+        number.textColor = isOn ? .white : .black;
+        darkModeLabel.textColor = isOn ? .white : .black;
+
+        darkModeSwitch.isOn = isOn;
     }
 
 
